4eec1e52bb89ac721679baca3022945cee1850cf
xiachanzou <xiachanzou@outlook.com>
2018-06-15 14:34:07 +0800

    添加排序工具类支持

---
+++ src/main/java/com/owl/magicUtil/util/ReorderUtil.java
@@ -0,0 +1,45 @@
+package com.owl.magicUtil.util;
+
+/**
+ * author engwen
+ * email xiachanzou@outlook.com
+ * time 2018/06/15.
+ */
+
+import com.owl.magicUtil.model.MsgResult;
+import com.owl.magicUtil.vo.ReorderVO;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class ReorderUtil<T extends MsgResult> {
+    public List<T> getSort(List<ReorderVO<T>> reorderVOList) {
+        List<T> result = new ArrayList<>();
+        reorderVOList.sort(new ReorderRule());
+        reorderVOList.forEach(reorderVO -> result.add(reorderVO.getResultData()));
+        return result;
+    }
+}
+
+
+class ReorderRule implements Comparator<ReorderVO> {
+    @Override
+    public int compare(ReorderVO o1, ReorderVO o2) {
+        int flag = 0;
+        //含有为空的排序，则默认将其后置
+        if (null == o1.getOrder() && null != o2.getOrder()) {
+            flag = -1;
+        } else if (null != o1.getOrder() && null == o2.getOrder()) {
+            flag = 1;
+        } else if (null != o1.getOrder() && null != o2.getOrder()) {
+            //都不为空按照大小排序
+            if (o1.getOrder() - o2.getOrder() > 0) {
+                flag = 1;
+            } else if (o1.getOrder() - o2.getOrder() < 0) {
+                flag = -1;
+            }
+        }
+        return flag;
+    }
+}
---
+++ src/main/java/com/owl/magicUtil/vo/ReorderVO.java
@@ -0,0 +1,35 @@
+package com.owl.magicUtil.vo;
+
+import com.owl.magicUtil.model.MsgResult;
+
+/**
+ * 排序基类
+ * author engwen
+ * email xiachanzou@outlook.com
+ * time 2018/06/15.
+ */
+public final class ReorderVO<T extends MsgResult> extends MsgResult {
+    //序号
+    private Integer order;
+
+    public Integer getOrder() {
+        return order;
+    }
+
+    //禁止无参
+    private ReorderVO() {
+
+    }
+
+    //初始化对象
+    public ReorderVO(Integer order, T object) {
+        this.order = order;
+        super.setResultData(object);
+    }
+
+    @Override
+    @SuppressWarnings("unchecked")
+    public T getResultData() {
+        return null != super.getResultData() ? (T) super.getResultData() : null;
+    }
+}
